7e3b2344cbe78fb6f11d38b3703c6f5fbf1cd984
ksusuhyun <100765937+ksusuhyun@users.noreply.github.com>
2024-05-13 16:20:47 +0900

    Title: 십의 자리 숫자, Time: 101ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240513/십의 자리 숫자/number-of-tens-digit.py
+++ 240513/십의 자리 숫자/number-of-tens-digit.py
@@ -1,6 +1,9 @@
 num = list(map(int,input().split()))
 cnt = [0] * 10
 for i in num:
-    cnt[i//10] += 1
+    if num != 0:
+        cnt[i//10] += 1
+    else:
+        break
 for j in range(1,10):
     print(f'{j} - {cnt[j]}')
