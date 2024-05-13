9a392826a0bab4e83f6ea478f5e3b89aed07176b
ksusuhyun <100765937+ksusuhyun@users.noreply.github.com>
2024-05-14 00:25:37 +0900

    Title: 빙빙 돌며 숫자 사각형 채우기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240514/빙빙 돌며 숫자 사각형 채우기/snail-number-square.py
+++ 240514/빙빙 돌며 숫자 사각형 채우기/snail-number-square.py
@@ -9,17 +9,17 @@ dd = {'U':0,'R':1,'D':2,'L':3}
 dx, dy = [-1,0,1,0], [0,1,0,-1]
 
 x,y= 0, 0
+d2[x][y] = 1
 d_num = 1
 for i in range(1,n*m+1):
-    if in_range(x,y) and d2[x][y] == 0:
-        d2[x][y] = i
-        x, y = x+dx[d_num], y+dy[d_num]
-    else :
-        x, y = x-dx[d_num],y-dy[d_num]
+    nx,ny = x+dx[d_num], y+dy[d_num]
+
+    if not in_range(nx,ny) and d2[nx][ny] == 0:
         d_num = (d_num+1)%4
-        x, y = x+dx[d_num], y+dy[d_num]
-        d2[x][y] = i
-        x, y = x+dx[d_num], y+dy[d_num]
+
+    x, y = x+dx[d_num], y+dy[d_num]
+    d2[x][y] = i
+    
 for k in d2:
     for h in k:
         print(h, end=" ")
