2123d1b99757eb3054be1124f92414f693c57d5f
ksusuhyun <100765937+ksusuhyun@users.noreply.github.com>
2024-05-13 16:32:26 +0900

    Title: 점수대 파악하기, Time: 109ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240513/점수대 파악하기/find-out-the-score-range.py
@@ -0,0 +1,9 @@
+score = list(map(int,input().split()))
+cnt = [0]*11
+for i in score:
+    if i != 0:
+        cnt[i//10] += 1
+    else:
+        break
+for j in range(10,0,-1):
+    print(f'{j*10} - {cnt[j]}')
