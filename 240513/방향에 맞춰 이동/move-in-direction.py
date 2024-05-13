6f282e34267cfddf66b64bb549cfe2fd30e5b8f0
ksusuhyun <100765937+ksusuhyun@users.noreply.github.com>
2024-05-13 19:22:58 +0900

    Title: 방향에 맞춰 이동, Time: 121ms, Memory: 24MB, Status: Passed - CodeTree

--- 240513/방향에 맞춰 이동/move-in-direction.py
+++ 240513/방향에 맞춰 이동/move-in-direction.py
@@ -1,26 +1,14 @@
 n = int(input())
-dx, dy = [0,0,0,0], [0,0,0,0]
-dd = []
+x, y = 0, 0
 for i in range(n):
     d, w = input().split()
     w = int(w)
-    dd.append(d)
     if d == 'S':
-        dy[0] = -1*w
+        y -= w
     elif d == 'N':
-        dy[1] = w
+        y += w
     elif d == 'E':
-        dx[2] = w
+        x += w
     else:
-        dx[3] = -1*w
-x, y = 0, 0
-for i in dd:
-    if i == 'S':
-        y = y + dy[0]
-    elif i == 'N':
-        y = y + dy[1]
-    elif i == 'E':
-        x = x + dx[2]
-    else:
-        x = x + dx[3]
+        x -= w
 print(x,y)
