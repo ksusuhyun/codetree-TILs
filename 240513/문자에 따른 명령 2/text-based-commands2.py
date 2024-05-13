75c0b0288661492b7d9d61ec2d1d171b09b030f9
ksusuhyun <100765937+ksusuhyun@users.noreply.github.com>
2024-05-13 19:47:57 +0900

    Title: 문자에 따른 명령 2, Time: 163ms, Memory: 26MB, Status: Passed - CodeTree

---
+++ 240513/문자에 따른 명령 2/text-based-commands2.py
@@ -0,0 +1,13 @@
+s = input()
+s = list(s)
+dx,dy =[0,1,0,-1],[1,0,-1,0]
+x, y = 0,0
+current = 0
+for i in s:
+    if i == 'L':
+        current = (current + 4 - 1) % 4
+    elif i == 'R':
+        current = (current + 1) % 4
+    else:
+        x,y = x+dx[current], y+dy[current]
+print(x, y)
