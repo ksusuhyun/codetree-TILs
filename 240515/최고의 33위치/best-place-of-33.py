5ff8ee5a8e20c4a99448569b05956234faed5ec9
ksusuhyun <100765937+ksusuhyun@users.noreply.github.com>
2024-05-15 20:10:02 +0900

    Title: 최고의 33위치, Time: 134ms, Memory: 24MB, Status: Passed - CodeTree

--- 240515/최고의 33위치/best-place-of-33.py
+++ 240515/최고의 33위치/best-place-of-33.py
@@ -1,10 +1,10 @@
 n = int(input())
 grid = [list(map(int,input().split())) for _ in range(n)]
 max_coin = 0
-for i in range(n-2):
+for i in range(n-2): # n-2로 해주는 이유: 3x3 격자가 nxn 격자를 벗어나지 않기 위해
     for j in range(n-2):
         coin = 0
-        for k in grid[i:i+3][j:j+3]:
-            coin += k.count(1)
+        for m in range(i,i+3):
+            coin += grid[m][j:j+3].count(1)
         max_coin = max(max_coin, coin)
 print(max_coin)
