8fe3f6cfa4a9424002616c6462117b8bc0930d8f
ksusuhyun <100765937+ksusuhyun@users.noreply.github.com>
2024-05-15 21:36:41 +0900

    Title: 숫자가 더 큰 인접한 곳으로 이동, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240515/숫자가 더 큰 인접한 곳으로 이동/move-to-larger-adjacent-cell.py
+++ 240515/숫자가 더 큰 인접한 곳으로 이동/move-to-larger-adjacent-cell.py
@@ -6,14 +6,12 @@ dx, dy = [-1,1,0,0], [0,0,-1,1]
 def in_range(x,y):
     return 0 <= x and x < n and 0 <= y and y < n
 
-ox, oy = x, y
 num = [grid[x][y]]
 while True:
     for i in range(4):
         nx, ny = x+dx[i], y+dy[i]
         if in_range(nx,ny) and grid[x][y] < grid[nx][ny] :
             num.append(grid[nx][ny])
-            ox, oy = x, y
             x, y = nx, ny
             break
         ox, oy = x, y
