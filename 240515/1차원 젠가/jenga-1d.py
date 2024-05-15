336e425017acaa967cbe227dc44b604d1dfe17de
ksusuhyun <100765937+ksusuhyun@users.noreply.github.com>
2024-05-15 20:42:53 +0900

    Title: 1차원 젠가, Time: 116ms, Memory: 24MB, Status: Passed - CodeTree

--- 240515/1차원 젠가/jenga-1d.py
+++ 240515/1차원 젠가/jenga-1d.py
@@ -3,9 +3,18 @@ jenga = [int(input()) for _ in range(n)]
 s1,e1 = map(int,input().split())
 s2,e2 = map(int,input().split())
 
-del jenga[s1-1:e1]
-del jenga[s2-1:e2]
+def take_out(jenga,s,e):
+    for i in range(s-1,e):
+        jenga[i] = 0
+    temp = []
+    for j in jenga:
+        if j != 0:
+            temp.append(j)
+    jenga = temp
+    return jenga
 
-print(len(jenga))
-for j in jenga:
+jenga = take_out(jenga,s1,e1)
+res = take_out(jenga,s2,e2)
+print(len(res))
+for j in res:
     print(j)
