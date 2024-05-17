529abc03e26996057f94f2501774c4ff14873633
ksusuhyun <100765937+ksusuhyun@users.noreply.github.com>
2024-05-17 20:22:16 +0900

    Title: 숫자가 가장 큰 인접한 곳으로 동시에 이동, Time: 132ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240517/숫자가 가장 큰 인접한 곳으로 동시에 이동/move-to-max-adjacent-cell-simultaneously.py
+++ 240517/숫자가 가장 큰 인접한 곳으로 동시에 이동/move-to-max-adjacent-cell-simultaneously.py
@@ -6,9 +6,7 @@ for _ in range(m):
     r, c = r-1, c-1
     glass[r][c] = 1
 
-# print(glass)
-
-dx, dy = [1,-1,0,0], [0,0,-1,1]
+dx, dy = [-1,1,0,0], [0,0,-1,1]
 
 def in_range(x,y):
     return 0 <= x and x < n and 0 <= y and y < n
@@ -20,7 +18,6 @@ for _ in range(t):
                 for i in range(4):
                     nx, ny = x + dx[i], y + dy[i]
                     if in_range(nx,ny) and grid[x][y] < grid[nx][ny] :
-                        # print(x,y,nx,ny)
                         glass[x][y] = 0
                         glass[nx][ny] += 1
                         break
