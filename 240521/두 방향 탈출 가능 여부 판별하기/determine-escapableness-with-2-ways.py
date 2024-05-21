6a958200f4f30470bf200dfb7856d015d19ed21d
ksusuhyun <100765937+ksusuhyun@users.noreply.github.com>
2024-05-21 21:13:35 +0900

    Title: 두 방향 탈출 가능 여부 판별하기, Time: 216ms, Memory: 27MB, Status: Passed - CodeTree

--- 240521/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.py
+++ 240521/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.py
@@ -7,21 +7,27 @@ visited = [ [False for _ in range(m)] for _ in range(n) ]
 def in_range(x,y):
     return 0 <= x and x < n and 0 <= y and y < m
 
-dx, dy = [1,0],[0,1]
-def dfs(vertex):
-    x, y = vertex[0], vertex[1]
+def can_go(x, y):
+    if not in_range(x,y):
+        return False
+    if visited[x][y] or grid[x][y] == 0:
+        return False
+    else:
+        return True
 
+dx, dy = [1,0],[0,1]
+def dfs(x,y):
     for i in range(2):
         nx, ny = x+dx[i], y+dy[i]
 
-        if in_range(nx,ny) and grid[nx][ny] != 0 and not visited[nx][ny]:
-            dfs((nx,ny))
+        if can_go(nx,ny):
+            visited[nx][ny] = True
+            dfs(nx,ny)
 
-start = (0,0)
 visited[0][0] = True
-dfs(start)
+dfs(0,0)
 
-if visited[m-1][m-1]:
+if visited[n-1][m-1]:
     print(1)
 else:
     print(0)
