49e69ba63d7dc3c755d17226989c187427cca76d
ksusuhyun <100765937+ksusuhyun@users.noreply.github.com>
2024-05-22 17:08:40 +0900

    Title: 그래프 탐색, Time: 105ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240522/그래프 탐색/graph-traversal.py
@@ -0,0 +1,34 @@
+N, M = map(int,input().split())
+# 인접 행렬
+graph = [ [0 for _ in range(N+1)] for _ in range(N+1) ]
+visited = [ False for _ in range(N+1)]
+
+for _ in range(M):
+    x, y = map(int,input().split())
+    graph[x][y] = 1
+
+def dfs(curr_v):
+    for next_v in range(1,N+1):
+        if graph[curr_v][next_v] and not visited[curr_v]:
+            visited[curr_v] = True
+            dfs(next_v)
+
+dfs(1)
+print(graph[1].count(1))
+
+# # 인접 리스트
+# graph = [ [] for _ in range(N+1) ] # 1~N 번으로 정점 주어짐 => N+1개 만들어야 함
+# visited = [ 0 for _ in range(N+1) ]
+
+# for _ in range(M):
+#     x,y = map(int,input().split())
+#     graph[x].append(y)
+#     graph[y].append(x)
+
+# def dfs(vertex):
+#     for curr_v in graph[vertex]:
+#         if visited[curr_v] == 0:
+#             visited[curr_v] = 1
+#             dfs(curr_v)
+
+# print(graph[1])
