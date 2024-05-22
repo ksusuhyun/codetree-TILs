8d948fe62abe48ae4bf99adacbaf8cc862a1389f
ksusuhyun <100765937+ksusuhyun@users.noreply.github.com>
2024-05-22 17:33:27 +0900

    Title: 그래프 탐색, Time: 197ms, Memory: 26MB, Status: Passed - CodeTree

--- 240522/그래프 탐색/graph-traversal.py
+++ 240522/그래프 탐색/graph-traversal.py
@@ -1,36 +1,37 @@
 N, M = map(int,input().split())
 # 인접 행렬
-graph = [ [0 for _ in range(N+1)] for _ in range(N+1) ]
-visited = [ False for _ in range(N+1)]
-
-for _ in range(M):
-    x, y = map(int,input().split())
-    graph[x][y] = 1
-    graph[y][x] = 1
-
-def dfs(curr_v):
-    for next_v in range(1,N+1):
-        if graph[curr_v][next_v] and not visited[next_v]:
-            visited[next_v] = True
-            dfs(next_v)
-
-dfs(1)
-visited[1] = True
-print(visited.count(True)-1)
-
-# # 인접 리스트
-# graph = [ [] for _ in range(N+1) ] # 1~N 번으로 정점 주어짐 => N+1개 만들어야 함
-# visited = [ 0 for _ in range(N+1) ]
+# graph = [ [0 for _ in range(N+1)] for _ in range(N+1) ]
+# visited = [ False for _ in range(N+1)]
 
 # for _ in range(M):
-#     x,y = map(int,input().split())
-#     graph[x].append(y)
-#     graph[y].append(x)
+#     x, y = map(int,input().split())
+#     graph[x][y] = 1
+#     graph[y][x] = 1
 
-# def dfs(vertex):
-#     for curr_v in graph[vertex]:
-#         if visited[curr_v] == 0:
-#             visited[curr_v] = 1
-#             dfs(curr_v)
+# def dfs(curr_v):
+#     for next_v in range(1,N+1):
+#         if graph[curr_v][next_v] and not visited[next_v]:
+#             visited[next_v] = True
+#             dfs(next_v)
 
-# print()
+# dfs(1)
+# visited[1] = True
+# print(visited.count(True)-1)
+
+# 인접 리스트
+graph = [ [] for _ in range(N+1) ] # 1~N 번으로 정점 주어짐 => N+1개 만들어야 함
+visited = [ 0 for _ in range(N+1) ]
+
+for _ in range(M):
+    x,y = map(int,input().split())
+    graph[x].append(y)
+    graph[y].append(x)
+
+def dfs(curr_v):
+    for next_v in graph[curr_v]:
+        if visited[next_v] == 0:
+            visited[next_v] = 1
+            dfs(next_v)
+dfs(1)
+visited[1] = 1
+print(visited.count(1)-1)
