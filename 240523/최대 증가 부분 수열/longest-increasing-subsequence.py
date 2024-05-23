6d5790cbc8fab38f11a1bdd24efe59fc8f8e11ca
ksusuhyun <100765937+ksusuhyun@users.noreply.github.com>
2024-05-23 21:50:30 +0900

    Title: 최대 증가 부분 수열, Time: 179ms, Memory: 25MB, Status: Passed - CodeTree

--- 240523/최대 증가 부분 수열/longest-increasing-subsequence.py
+++ 240523/최대 증가 부분 수열/longest-increasing-subsequence.py
@@ -1,7 +1,7 @@
 n = int(input())
 num = list(map(int,input().split()))
-dp = [ 0 for _ in range(n) ]
-dp[0] = 1
+dp = [ 1 for _ in range(n) ]
+
 for i in range(1,n):
     for j in range(0,i):
         # if dp[j] == 0:
