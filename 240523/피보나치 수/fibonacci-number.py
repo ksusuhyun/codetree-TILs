6542c619e70efecdf4b3619ae46721b55fd7f0d0
ksusuhyun <100765937+ksusuhyun@users.noreply.github.com>
2024-05-23 19:53:16 +0900

    Title: 피보나치 수, Time: 137ms, Memory: 24MB, Status: Passed - CodeTree

--- 240523/피보나치 수/fibonacci-number.py
+++ 240523/피보나치 수/fibonacci-number.py
@@ -1,8 +1,23 @@
 n = int(input())
 
 # 1.tabulation
-num = [0,1,1]
-for i in range(3,n+1):
-    num.append(num[i-1] + num[i-2])
+# num = [0,1,1]
+# for i in range(3,n+1):
+#     num.append(num[i-1] + num[i-2])
     
-print(num[n])
+# print(num[n])
+
+# 2.memoization
+memo = [-1]*(n+1)
+def fibo(n):
+    if memo[n] != -1:
+        return memo[n]
+    if n <= 2:
+        memo[n] = 1
+        return 1
+
+    memo[n] = fibo(n-1) + fibo(n-2)
+    return memo[n]
+
+fibo(n)
+print(memo[-1])
