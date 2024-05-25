e1d384e9a1d491b5d74de61474bd2f7dcf05d29b
ksusuhyun <100765937+ksusuhyun@users.noreply.github.com>
2024-05-25 17:59:58 +0900

    Title: 연결된 그래프 2, Time: 922ms, Memory: 75MB, Status: Wrong Answer - CodeTree

--- 240525/연결된 그래프 2/connected-graph-2.py
+++ 240525/연결된 그래프 2/connected-graph-2.py
@@ -1,30 +1,39 @@
 n, m = map(int,input().split())
 graph = [ [] for _ in range(n+1) ]
 for _ in range(m):
-    c, n = map(int,input().split())
-    graph[c].append(n)
-visited = [ False for _ in range(n+1) ]
+    s,e = map(int,input().split())
+    graph[s].append(e)
+visited = [ 0 for _ in range(n+1) ]
 
 memo = [ -1 for _ in range(n+1) ]
 def dfs(curr):
-    if visited[curr]:
+    if visited[curr] == 2:
+        return memo[curr]
+
+    if visited[curr] == 1:
         return memo[curr]
 
     if len(graph[curr]) == 0:
         memo[curr] = 0
-        visited[curr] = True
+        visited[curr] = 2
         return memo[curr]
+
+    visited[curr] = 1
+    memo[curr] = 0
 
     for next_ in graph[curr]:
         memo[curr] = max(memo[curr],dfs(next_)+1)
 
-    visited[curr] = True
+    visited[curr] = 2
     return memo[curr]
 
+res = []
 for i in range(1,n+1):
-    dfs(i)
+    res.append(dfs(i))
+    visited = [ 0 for _ in range(n+1) ]
+    memo = [ -1 for _ in range(n+1) ]
 
-maxx = max(memo)
-for i in range(len(memo)):
-    if memo[i] == maxx:
-        print(i, end=" ")
+maxx = max(res)
+for i in range(len(res)):
+    if res[i] == maxx:
+        print(i+1, end=" ")
